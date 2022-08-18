a8d6779ab2b18178e581e7d669dbee2b6fd7554a
Alex Taboada <alexandrotaboada@gmail.com>
2022-08-18 16:26:59 -0300

    Token deploy

--- scripts/deployToken.ts
+++ scripts/deployToken.ts
@@ -2,18 +2,19 @@ import { ethers } from "hardhat";
 import dotenv from "dotenv"
 dotenv.config()
 
-export const deployContract = async () => {
+export const deployToken = async () => {
   const TOKEN_NAME:string = process.env.TOKEN_NAME || ""
   const TOKEN_SYMBOL:string = process.env.TOKEN_SYMBOL || ""
 
   const Contract = await ethers.getContractFactory("Token");
   const token = await Contract.deploy(TOKEN_NAME, TOKEN_SYMBOL);
 
+  console.log("Token deployed to: " + token.address)
   return token.deployed();
 }
 
 async function main() {
-  await deployContract();
+  await deployToken();
 }
 
 main().catch((error) => {
